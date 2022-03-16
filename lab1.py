ba610019524d7d36dbe96ee254db8112762a8e55
Łukasz Moszczyński <92304821+XanDuri@users.noreply.github.com>
2022-03-16 13:00:22 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,56 @@
+# #zad1
+# a=1
+# b=1.2
+# c='Slowo'
+# d='zdanie zlozone ze slow'
+# e='123456789'
+# wynik=a+b
+# f=1+10j
+# #zad2
+# a=input('Podaj liczba a:')
+# a=int(a)
+# b=input('Podaj liczbe b:')
+# b=int(b)
+# znak=input('Podaj znak dzialania(-,+,*,/): ')
+# if znak=='-':
+#     wynik=a-b
+#     print(wynik)
+# elif znak=='+':
+#     wynik=a+b
+#     print(wynik)
+# elif znak == '*':
+#     wynik=a*b
+#     print(wynik)
+# elif znak=='/':
+#     wynik=a/b
+#     print(wynik)
+# else:
+#     print('Podano zly znak')
+#zad3
+# a=5
+# b=6
+# a+=b
+# print(a)
+# a-=b
+# print(a)
+# a*=b
+# print(a)
+# a/=b
+# print(a)
+# a**=b
+# print(a)
+# a%=b
+# print(a)
+# #zad4
+# import math
+# print(math.e**10)
+# print(math.sqrt(2))
+#zad5
+
+#zad6
+
+#zad7
+
+#zad8
+
+#zad9
